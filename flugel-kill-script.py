bfcee8482e73e42834e33320ad74cb0ca1634140
Espyz <truharandrey1015@gmail.com>
2025-04-11 20:45:41 +0500

    Убрал движение мыши

--- flugel-kill-script.py
+++ flugel-kill-script.py
@@ -13,10 +13,10 @@ def toggle_mouse_movement():
     else:
         print("Движение мыши выключено")
 
-# Привязываем комбинацию Ctrl+Z к функции переключения
+# Привязываем комбинацию Alt+Z к функции переключения
 keyboard.on_press_key("z", lambda e: toggle_mouse_movement() if keyboard.is_pressed("alt") else None)
 
-print("Программа запущена. Нажмите Ctrl+Z для включения/выключения движения мыши")
+print("Программа запущена. Нажмите Alt+Z для включения/выключения движения мыши. Убедитесь, что на вашем дк нагруднике 200% верт.ускорение и авт.вкл.полета")
 print("Для выхода полностью закройте программу")
 iter=0
 # print(pyautogui.KEY_NAMES)
@@ -26,7 +26,10 @@ iter=0
 while True:
     if running:
         # Убиваем Флюгелей
-        pyautogui.moveRel(0, -5000, duration=0.32)
+        pyautogui.keyDown('space')
+        time.sleep(0.1)
+        pyautogui.keyUp('space')
+
         for i in range (1, 8):
             pyautogui.press(str(i))
             pyautogui.click(clicks=8, interval=0.1)
@@ -34,23 +37,10 @@ while True:
         # Ожидаем перезарядки убийц флюгеля
         time.sleep(10)
 
-        # Набираем Души флюгеля
-        if (iter % 20 == 0):
-            pyautogui.press('8', interval=0.5)
-            time.sleep(0.1)
-            pyautogui.click(button='right', duration=0.1)
-            pyautogui.moveRel(0, -440, duration=0.3)
-            pyautogui.click(duration=0.1)
-            pyautogui.write('@alfheim', interval=0.1)
-            pyautogui.moveRel(-170, 115, duration=0.3)
-            pyautogui.keyDown('shift')
-            pyautogui.click(clicks=20, interval=0.1)
-            pyautogui.keyUp('shift')
-            pyautogui.press('esc')
         iter += 1
 
         # Убираем сломавшуюся душу
-        if ((iter > 1) and iter % 2 == 1):
+        if ((iter > 1) and iter % 2 == 0):
             #Попытка удалять через терминал
             pyautogui.press(str(8))
             pyautogui.click(button='right', duration=0.1)
@@ -60,13 +50,19 @@ while True:
             pyautogui.click(duration=0.1)
             pyautogui.moveRel(17, -17, duration=0.3)
             pyautogui.click(duration=0.1)
+            pyautogui.moveRel(0, -700, duration=0.3)
+            pyautogui.click(duration=0.1)
+            pyautogui.write('@alfheim', interval=0.1)
+            pyautogui.moveRel(-170, 115, duration=0.3)
+            pyautogui.keyDown('shift')
+            pyautogui.click(clicks=2, interval=0.1)
+            pyautogui.keyUp('shift')
             pyautogui.press('esc')
 
-
         # Спавн Флюгелей
-        pyautogui.moveRel(0, 5000, duration=0.32)
         pyautogui.press('9')
         pyautogui.keyDown('shift')
+        time.sleep(0.5)
         pyautogui.click(clicks=7, interval=1, button='right')
         pyautogui.keyUp('shift')
     else:
